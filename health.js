65d623e563dda289526645a536119931157e70d8
Shivam Sharma <sdsharma@ucsd.edu>
2016-02-26 21:26:54 -0800

    Finally reading some dank jawbone data

--- health.js
+++ health.js
@@ -20,6 +20,10 @@ var app = express();
   tokenURL: 'https://jawbone.com/auth/oauth2/token',
   callbackURL: 'https://localhost:3000/login/jawbone/callback'
 };
+var demoSession = {
+    accessToken: '',
+    refreshToken: ''
+};
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -29,77 +33,72 @@ var sslOptions = {
   key: fs.readFileSync('./server.key'),
   cert: fs.readFileSync('./server.crt')
 };
-passport.use('jawbone', new JawboneStrategy({
-  clientID: jawboneAuth.clientID,
-  clientSecret: jawboneAuth.clientSecret,
-  authorizationURL: jawboneAuth.authorizationURL,
-  tokenURL: jawboneAuth.tokenURL,
-  callbackURL: jawboneAuth.callbackURL
-}, function(token, refreshToken, profile, done) {
-  options = {
-        access_token: token,
-        client_id: jawboneAuth.clientID,
-        client_secret: jawboneAuth.clientSecret
-      },
-      up = require('jawbone-up')(options);
-
-  up.sleeps.get({}, function(err, body) {
-    if (err) {
-      console.log('Error receiving Jawbone UP data');
-    } else {
-      var jawboneData = JSON.parse(body).data;
-      console.log(jawboneData);
-      for (var i = 0; i < jawboneData.items.length; i++) {
-        var date = jawboneData.items[i].date.toString(),
-            year = date.slice(0,4),
-            month = date.slice(4,6),
-            day = date.slice(6,8);
-
-        jawboneData.items[i].date = day + '/' + month + '/' + year;
-        jawboneData.items[i].title = jawboneData.items[i].title.replace('for ', '');
-      }
-
-      return done(null, jawboneData, console.log('Jawbone UP data ready to be displayed.'));
-    }
-  });
-}));
+// passport.use('jawbone', new JawboneStrategy({
+//   clientID: jawboneAuth.clientID,
+//   clientSecret: jawboneAuth.clientSecret,
+//   authorizationURL: jawboneAuth.authorizationURL,
+//   tokenURL: jawboneAuth.tokenURL,
+//   callbackURL: jawboneAuth.callbackURL
+// }, function(accessToken, refreshToken, profile, done) {
+//     // we got the access token, store it in our temp session
+//     demoSession.accessToken = accessToken;
+//     demoSession.refreshToken = refreshToken;
+//     var user = {};
+//     done(null, user);
+//     console.dir(demoSession);
+// }));
 
 app.get('/login/jawbone', 
   passport.authorize('jawbone', {
-    scope: ['move_read','calories'],
+    scope: ['basic_read','sleep_read'],
     failureRedirect: './'
   })
 );
-app.get('/login/jawbone/callback',
-  passport.authorize('jawbone', {
-    scope: ['move_read','calories'],
-    failureRedirect: '/'
-  }), function(req, res) {
-    res.render('userdata', req.account);
-    up.moves.get({}, function(err, body) {
-    if (err) {
-      console.log('Error receiving Jawbone UP data');
-    } else {
-      var jawboneData = JSON.parse(body).data;
-      console.log(jawboneData);
-      for (var i = 0; i < jawboneData.items.length; i++) {
-        var date = jawboneData.items[i].date.toString(),
-            year = date.slice(0,4),
-            month = date.slice(4,6),
-            day = date.slice(6,8);
-
-        jawboneData.items[i].date = day + '/' + month + '/' + year;
-        jawboneData.items[i].title = jawboneData.items[i].title.replace('for ', '');
-      }
-
-      // return done(null, jawboneData, console.log('Jawbone UP data ready to be displayed.'));
-      console.log("here");
-      console.log(jawboneData);
-
+app.get('/login/jawbone/callback', passport.authorize('jawbone', {
+        scope: ['basic_read','sleep_read'],
+        failureRedirect: '/'
+    }), function(req, res) {
+        res.redirect('/userdata');
     }
-  });
-  }
 );
+app.get('/userdata', function(req, res) {
+
+    var options = {
+        access_token: demoSession.accessToken,
+        client_id: jawboneAuth.clientID,
+        client_secret: jawboneAuth.clientSecret
+    };
+    var up = require('jawbone-up')(options);
+
+    // we need to add date or sleep call fails
+    var yyyymmdd = (new Date()).toISOString().slice(0, 10).replace(/-/g, "");
+    console.log('Getting sleep for day ' + yyyymmdd);
+
+    up.sleeps.get({date:yyyymmdd}, function(err, body) {
+        if (err) {
+            console.log('Error receiving Jawbone UP data');
+            return res.render('userdata', {
+                    requestTime: 0,
+                    jawboneData: 'Error result'
+                });
+        } else {
+            try {
+                var result = JSON.parse(body);
+                console.log(result);
+                res.render('userdata', {
+                    requestTime: result.meta.time,
+                    jawboneData: JSON.stringify(result.data)
+                });
+            }
+            catch(err) {
+                res.render('userdata', {
+                    requestTime: 0,
+                    jawboneData: 'Unknown result'
+                });
+            }
+        }
+    });
+});
 //server
 var usersFilePath = path.join(__dirname, '/views/macrosdata.json');
  app.get("/", function(req, res) {
@@ -118,7 +117,20 @@ var usersFilePath = path.join(__dirname, '/views/macrosdata.json');
  app.get(/^(.+)$/, function(req, res){ 
      res.sendfile( __dirname + "/views/" + req.params[0]); 
  });
-
+passport.use('jawbone', new JawboneStrategy({
+    clientID: jawboneAuth.clientID,
+    clientSecret: jawboneAuth.clientSecret,
+    authorizationURL: jawboneAuth.authorizationURL,
+    tokenURL: jawboneAuth.tokenURL,
+    callbackURL: jawboneAuth.callbackURL
+}, function(accessToken, refreshToken, profile, done) {
+    // we got the access token, store it in our temp session
+    demoSession.accessToken = accessToken;
+    demoSession.refreshToken = refreshToken;
+    var user = {};
+    done(null, user);
+    console.dir(demoSession);
+}));
  /*var secureServer =*/ https.createServer(sslOptions, app).listen(3000, function(){
   console.log('UP server listening on ' + 3000);
 });
